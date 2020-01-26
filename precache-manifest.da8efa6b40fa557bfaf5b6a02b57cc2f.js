3c2836f916f92fd7d61cb8e7a0ed4a567a0a5716
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2020-01-26 12:57:32 -0600

    Updates

---
+++ precache-manifest.da8efa6b40fa557bfaf5b6a02b57cc2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f080b3c57b9e8205e248117663c15004",
+    "url": "/insurance-react-practices/index.html"
+  },
+  {
+    "revision": "e5365bd12f57bc321cc4",
+    "url": "/insurance-react-practices/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "ee151c62a88e55f6afc4",
+    "url": "/insurance-react-practices/static/js/2.0bea2ab4.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/insurance-react-practices/static/js/2.0bea2ab4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e5365bd12f57bc321cc4",
+    "url": "/insurance-react-practices/static/js/main.4ef76e21.chunk.js"
+  },
+  {
+    "revision": "6b6d85174cc2b40eda9a",
+    "url": "/insurance-react-practices/static/js/runtime-main.3eec98ac.js"
+  }
+]);
